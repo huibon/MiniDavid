19f4a1a6ae5478c4b05e238bfd5143ce85efe8c3
David Lu <david.y.lu2008@gmail.com>
2012-08-29 23:01:57 -0400

    fefinition helper

--- applicationx/helpers/definitions_helper.php
+++ applicationx/helpers/definitions_helper.php
@@ -4,15 +4,11 @@
 
 $WEBROOT = "http://".$_SERVER['HTTP_HOST'].'/';
 $DOC = $WEBROOT."doc/";
-$TEMPLATE = $WEBROOT."template/";
-$LIB = $TEMPLATE."lib/";
-$IMG = $TEMPLATE."img/";
-$JS = $TEMPLATE."js/";
-$CSS = $TEMPLATE."css/";
+$IMG = $WEBROOT."images/";
+$JS = $WEBROOT."js/";
+$CSS = $WEBROOT."css/";
 
 define('WEBROOT', $WEBROOT);
-define('DOC', $DOC);
-define('TEMPLATE', $TEMPLATE);
 define('LIB', $LIB);
 define('IMG', $IMG);
 define('JS', $JS);
