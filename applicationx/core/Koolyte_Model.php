84bb8a884be933e6a641df78c53ce1ed92e70b4c
David Lu <lsdavid1983@hotmail.com>
2012-09-06 22:15:23 -0400

    model

---
+++ applicationx/core/Koolyte_Model.php
@@ -0,0 +1,18 @@
+<?php
+
+
+/**
+ * Parent model class for all M2W models.  Not used for anything
+ * yet, but allows us to set default variable values and function
+ * behaviours later on if we need to.
+ *
+ */
+
+class Koolyte_Model extends CI_Model {
+
+	function __construct() {
+		parent::__construct();
+	}
+}
+
+?>
