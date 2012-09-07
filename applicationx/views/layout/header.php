8c2aced69a7193784a1cd4949e291d0a97a05f20
Jason Fang <jason@catalystworkshop.com>
2012-09-06 22:56:21 -0400

    updated db setting

--- applicationx/views/layout/header.php
+++ applicationx/views/layout/header.php
@@ -1,5 +1,15 @@
-<div id="header">
+<!--<div id="header">
 	<div id="header_inner">
 		Hello world!
     </div>
+</div>-->
+
+<div class="header1bg">
+  <div class="header1">
+  <div class="header_logo"><a></a></div>
+  </div>
+</div>
+
+<div class="header2bg">
+
 </div>
