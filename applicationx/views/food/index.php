6105c6b58c45574fbeba60db895e38f8e4e40606
Jason Fang <jason@catalystworkshop.com>
2012-09-24 18:38:01 -0400

    food main page front-end update

--- applicationx/views/food/index.php
+++ applicationx/views/food/index.php
@@ -23,14 +23,192 @@
   </div>
 </div>
 
-<div class="site_search_bar_bg">
+<div class="site_search_bar_bg top_down_shadow_light">
   <div class="site_search_bar">
-
+    <div class="site_search_bar_menu">
+      <div class="site_search_bar_menu_item site_search_bar_menu_item_cuisine"></div>
+      <div class="site_search_bar_menu_item site_search_bar_menu_item_reputation"></div>
+      <div class="site_search_bar_menu_item site_search_bar_menu_item_area"></div>
+      <div class="site_search_bar_menu_item site_search_bar_menu_item_new"></div>
+      <div class="site_search_bar_menu_item site_search_bar_menu_item_topic"></div>
+    </div>
+    
+    <div class="site_search_bar_menu_2">
+      <div class="site_search_bar_menu_item site_search_bar_menu_item_map"></div>
+      <div class="site_search_bar_search"><input type="text" class="site_search_bar_input" id="site_search_bar_input_food" name="site_search_bar_input_food" maxlength="50" /></div>
+    </div>
   </div>
 </div>
 
+<div class="site_search_bar_sub_bg">
+  <div class="site_search_bar_sub">
+    <div class="site_search_bar_sub_left">所有菜系 ＋ 本月最火 ＋ 多伦多</div>
+    <div class="site_search_bar_sub_right">
+      <div class="site_search_bar_sub_right_item"><b>显示模式</b></div>
+      <div class="site_search_bar_sub_right_item">列表</div>
+      <div class="site_search_bar_sub_right_item">缩图</div>
+      <div class="site_search_bar_sub_right_item">地图模式</div>
+    </div>
+  </div>
+</div>
+
+<div class="site_pin_bg">
+  <script type="text/javascript" src="<?php echo base_url(); ?>js/jquery.masonry.min.js"></script>
+  <div class="site_pin_holder">
+    <div id="site_pin_container">
+    <?php  
+	for($i=1;$i<=22;$i+=1){
+	?>
+      <div class="site_pin_item center_shadow">
+        <div class="site_pin_item_img">
+          <img class="round_corner_3px" src="<?php echo base_url(); ?>images/temp/resto/resturant-temp-<?=$i;?>.jpg" />
+        </div>
+        <div class="site_pin_item_resto_title_bg">
+          <div class="site_pin_item_resto_title">餐馆 <?php echo rand(100, 999); ?></div>
+          <div class="site_pin_item_resto_cuisine">综合菜系</div>
+        </div>
+        <div class="site_pin_item_resto_desc">
+        餐馆简介在此显示...
+        </div>
+        <div class="site_pin_item_special">
+        招牌菜：珍宝鱼翅
+        </div>
+        <div class="site_pin_item_menu">
+          <div class="site_pin_item_menu_item">主页</div>
+          <div class="site_pin_item_menu_item">菜单</div>
+          <div class="site_pin_item_menu_item">优惠卷</div>
+          <div class="site_pin_item_menu_item tred">六周年酬宾会</div>
+        </div>
+        <div class="site_pin_item_stats">
+          <div class="site_pin_item_stats_item">推荐 <font class="tred">30</font></div>
+          <div class="site_pin_item_stats_item">品论 <font class="tred">12</font></div>
+          <div class="site_pin_item_stats_item">围观 <font class="tred">100</font></div>
+        </div>
+        <?php 
+		$reply_num = rand(1,4);
+		for($k=1;$k<=$reply_num;$k+=1){ ?>
+        <div class="site_pin_item_reply">
+        会员<?=$k?>：菜色不错，价格公道。
+        </div>
+        <?php } ?>
+        <div class="site_pin_item_reply"><input type="text" class="site_pin_item_quick_reply" id="resto_id_quick_reply" name="resto_id_quick_reply" value="comment" /></div>
+      </div>
+      <?php
+	  }
+	  ?>
+      <script>
+	  var $container = $('#site_pin_container');
+      $container.imagesLoaded(function(){
+        $container.masonry({
+          itemSelector : '.site_pin_item'
+        //columnWidth : 220
+        });
+      });
+	  </script>
+      <div class="clr"></div>
+    </div>
+  </div>
+</div>
+
+<div class="site_pagination_bg">
+    <div class="site_pagination">
+    <div class="site_pagination_item"><a href="#">1</a></div>
+    <div class="site_pagination_item"><a href="#">2</a></div>
+    <div class="site_pagination_item"><a href="#">3</a></div>
+    <div class="site_pagination_item"><a href="#">4</a></div>
+    <div class="site_pagination_item"><a href="#">5</a></div>
+    <div class="clr"></div>
+  </div>
+</div>
+
+<br /><br /><br />
+
+<div class="content_bg">
+  <div class="content">
+    <!-- text category -->
+    <div class="section_title">
+      <div class="section_title_text flt tred" style="width:100px;border-right:1px solid #ddd;">食系</div><div class="section_title_text flt">地区</div>
+      <div class="clr"></div>
+    </div>
+    <div class="site_content_1">
+      <ul class="site_content_ul">
+        <li><a href="#">粤菜</a></li>
+        <li><a href="#">港台菜</a></li>
+        <li><a href="#">湘菜</a></li>
+        <li><a href="#">苏菜</a></li>
+        <li><a href="#">北方菜</a></li>
+      </ul>
+      <ul class="site_content_ul">
+        <li><a href="#">川菜</a></li>
+        <li><a href="#">沪菜</a></li>
+        <li><a href="#">东南亚餐</a></li>
+        <li><a href="#">北美西餐</a></li>
+        <li><a href="#">法国</a></li>
+      </ul>
+      <ul class="site_content_ul">
+        <li><a href="#">德国</a></li>
+        <li><a href="#">印度</a></li>
+        <li><a href="#">意大利</a></li>
+        <li><a href="#">日本料理</a></li>
+        <li><a href="#">韩国料理</a></li>
+      </ul>
+      <ul class="site_content_ul">
+        <li><a href="#">越南美食</a></li>
+        <li><a href="#">酒吧</a></li>
+        <li><a href="#">奶茶小吃</a></li>
+        <li><a href="#">甜品店</a></li>
+        <li><a href="#">家庭厨房</a></li>
+      </ul>
+      <ul class="site_content_ul">
+        <li><a href="#">快餐系列</a></li>
+        <li><a href="#">早餐／午餐</a></li>
+        <li><a href="#">早茶</a></li>
+        <li><a href="#">素食</a></li>
+        <li><a href="#">生猛海鲜</a></li>
+      </ul>
+      
+      <ul class="site_content_ul">
+        <li><a href="#">火锅</a></li>
+        <li><a href="#">烧烤</a></li>
+        <li><a href="#">自助餐</a></li>
+        <li><a href="#">All You Can Eat</a></li>
+        <li><a href="#">其他</a></li>
+      </ul>
+      <div class="clr"></div>
+    </div>
+  </div>
+</div>
+
+<br /><br /><br />
+
+<div class="content_bg">
+  <div class="content">
+    <!-- weibo image -->
+    <div class="section_title">
+      <div class="section_title_text tred">食相</div>
+    </div>
+    <div class="site_social_image">
+      <div class="site_social_image_item">
+      <img src="<?php echo base_url(); ?>images/temp/social_food_image/temp_social_img_1.jpg" />
+      </div>
+      <div class="site_social_image_item">
+      <img src="<?php echo base_url(); ?>images/temp/social_food_image/temp_social_img_2.jpg" />
+      </div>
+      <div class="site_social_image_item">
+      <img src="<?php echo base_url(); ?>images/temp/social_food_image/temp_social_img_3.jpg" />
+      </div>
+      <div class="site_social_image_item">
+      <img src="<?php echo base_url(); ?>images/temp/social_food_image/temp_social_img_4.jpg" />
+      </div>
+    <div class="clr"></div>
+    </div>
+  </div>
+</div>
+
+<br /><br /><br />
 
 <script type="text/javascript">
+//temporary ads image
 $(".site_top_ads_item_1").css("background", "url(<?php echo base_url(); ?>images/temp/ads/temp_ads_1.jpg) no-repeat");
 $(".site_top_ads_item_2").css("background", "url(<?php echo base_url(); ?>images/temp/ads/temp_ads_2.jpg) no-repeat");
 $(".site_top_ads_item_3").css("background", "url(<?php echo base_url(); ?>images/temp/ads/temp_ads_3.jpg) no-repeat");
--- applicationx/views/layout/footer.php
+++ applicationx/views/layout/footer.php
@@ -1,5 +1,82 @@
-<div id="footer">
-	<div id="footer_inner">
-        
+<div class="footer_bg">
+  <div class="footer">
+    <div class="footer_section_1 flt">
+      <div class="footer_title twhite">合作伙伴</div>
+      <div class="footer_sub_title">PARTNERS</div>
+      <ul class="footer_ul">
+        <li><a href="#">网站A</a></li>
+        <li><a href="#">网站B</a></li>
+        <li><a href="#">网站C</a></li>
+        <li><a href="#">网站D</a></li>
+        <li><a href="#">公司A</a></li>
+        <li><a href="#">公司B</a></li>
+        <li><a href="#">公司C</a></li>
+        <li><a href="#">公司D</a></li>
+        <li><a href="#">公司E</a></li>
+        <li><a href="#">公司F</a></li>
+      </ul>
     </div>
+    <div class="footer_section_2 flt">
+      <div class="footer_section_2_sub flt">
+        <div class="footer_title tblue">关于我们</div>
+        <div class="footer_sub_title">ABOUT US</div>
+        <ul class="footer_ul">
+          <li><a href="#">酷来简介</a></li>
+          <li><a href="#">网站理念</a></li>
+          <li><a href="#">创建团队</a></li>
+          <li><a href="#">服务概念</a></li>
+          <li><a href="#">活动企划</a></li>
+        </ul>
+      </div>
+      <div class="footer_section_2_sub flt">
+        <div class="footer_title tblue">网站地图</div>
+        <div class="footer_sub_title">SITE MAP</div>
+        <ul class="footer_ul">
+          <li><a href="#">我的主页</a></li>
+          <li><a href="#">饮食</a></li>
+          <li><a href="#">房屋</a></li>
+          <li><a href="#">汽车</a></li>
+          <li><a href="#">玩乐</a></li>
+          <li><a href="#">黄页</a></li>
+          <li><a href="#">投资</a></li>
+          <li><a href="#">移民</a></li>
+          <li><a href="#">学业</a></li>
+          <li><a href="#">企业建站</a></li>
+          <li><a href="#">论坛</a></li>
+        </ul>
+      </div>
+      <div class="footer_section_2_sub flt">
+        <div class="footer_title tblue">社交</div>
+        <div class="footer_sub_title">SOCIAL</div>
+        <ul class="footer_ul">
+          <li><a href="#">Facebook</a></li>
+          <li><a href="#">Twitter</a></li>
+          <li><a href="#">新浪微博</a></li>
+          <li><a href="#">腾信微博</a></li>
+          <li><a href="#">官方博客</a></li>
+          <li><a href="#">最新活动</a></li>
+        </ul>
+        <br /><br />
+        <div class="footer_title tblue">联系我们</div>
+        <div class="footer_sub_title">CONTACT US</div>
+        <ul class="footer_ul">
+          <li><a href="#">电邮联系</a></li>
+          <li><a href="#">电话</a></li>
+          <li><a href="#">意见反馈</a></li>
+        </ul>
+      </div>
+      <div class="clr"></div>
+    </div>
+    <div class="footer_section_3 flt">
+      <div class="flr" style="height:260px;">
+      <a href="<?php echo base_url(); ?>"><img src="<?php echo base_url(); ?>images/site/temp_logo.png" /></a>
+      </div>
+      <div class="clr"></div>
+      <div class="flr" style="height:26px;font-size:10px;line-height:13px;color:#888;">
+      &copy;&nbsp;<?php echo date("Y");?> KOOLYTE <br />All Rights Reserved
+      </div>
+      <div class="clr"></div>
+    </div>
+    <div class="clr"></div>
+  </div>
 </div>
